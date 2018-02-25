1393173084fa99b207fdd718b300f96cda1a7aa7
subuta <shunta.3110@gmail.com>
2018-02-25 23:33:12 +0900

    Add getting started.

---
+++ server.js
@@ -0,0 +1,42 @@
+const next = require('next')
+const Koa = require('koa')
+const Router = require('koa-router')
+
+const port = parseInt(process.env.PORT, 10) || 3000
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({ dev })
+const handle = app.getRequestHandler()
+
+app.prepare()
+  .then(() => {
+    const server = new Koa()
+    const router = new Router();
+
+    router.get('/p/:id', async ctx => {
+      const actualPage = '/post'
+      const queryParams = { id: ctx.params.id }
+      await app.render(ctx.req, ctx.res, actualPage, queryParams)
+      ctx.respond = false
+    })
+
+    router.get('*', async ctx => {
+      await handle(ctx.req, ctx.res)
+      ctx.respond = false
+    })
+
+    server.use(async (ctx, next) => {
+      ctx.res.statusCode = 200
+      await next()
+    })
+
+    server.use(router.routes())
+
+    server.listen(port, (err) => {
+      if (err) throw err
+      console.log(`> Ready on http://localhost:${port}`)
+    })
+  })
+  .catch((ex) => {
+    console.error(ex.stack)
+    process.exit(1)
+  })
